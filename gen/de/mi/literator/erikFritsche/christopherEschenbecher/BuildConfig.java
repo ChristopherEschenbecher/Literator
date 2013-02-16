69334aa1d5d50dfdd2e15ccad5a0ae2d47f79c92
Christopher Eschenbecher <christopher.eschenbecher@googlemail.com>
2013-02-16 12:24:48 +0100

    blabla
    blaba

---
+++ gen/de/mi/literator/erikFritsche/christopherEschenbecher/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.mi.literator.erikFritsche.christopherEschenbecher;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
